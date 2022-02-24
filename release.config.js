6f2386c3733ef5edcc8a4116687711f563de1305
cuncis1st <cuncis1st@gmail.com>
2022-02-24 19:08:37 +0700

    update release

--- release.config.js
+++ release.config.js
@@ -5,13 +5,13 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
-        "@semantic-release/github",
-        {
-            assets: [
-                { path: "build.zip", label: "build" },
-                { path: "coverage.zip", label: "Coverage" }
-            ]
-        }
-    ]
-  ]
+      "@semantic-release/github",
+      {
+        assets: [
+          {path: "build.zip", label: "build"},
+          {path: "coverage.zip", label: "Coverage"},
+        ],
+      },
+    ],
+  ],
 };
